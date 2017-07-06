705ea1369be3324d46bb12faea8aaae7a434c35f
Zachary Lehman-Ludwig <zach.lehman-ludwig@ridewithvia.com>
2017-07-06 19:53:36 -0400

    Selection done

--- Attempt1.py
+++ Attempt1.py
@@ -11,8 +11,34 @@ connection = psy_connect(
 # alooma_read.merge(sandbox_read, how='inner', left_on=['rider_id', 'campaign_name'], right_on=['rider_id', 'campaign_name'])
 # pd.merge(alooma_read, sandbox_read, how='inner', on='rider_id',)
 
-sandbox_alooma = pd.read_sql("SELECT adwmain.alooma.* FROM adwmain.sandbox.campaign_eligibility "
-                             "RIGHT OUTER JOIN adwmain.alooma ON adwmain.alooma.rider_id = adwmain.sandbox.rider_id"
-                             "AND adwmain.alooma.campaign_group = adwmain.sandbox.campaign_group"
-                             "AND adwmain.alooma.campaign_name = adwmain.sandbox.campaign_name")
+sandbox_alooma = pd.read_sql("SELECT alooma.campaign_eligible.* FROM sandbox.campaign_eligibility "
+                             "RIGHT OUTER JOIN alooma.campaign_eligible ON alooma.campaign_eligible.rider_id = sandbox.campaign_eligibility.rider_id"
+                             " AND adwmain.alooma.campaign_eligible.campaign_group = adwmain.sandbox.campaign_eligibility.campaign_group"
+                             " AND adwmain.alooma.campaign_eligible.campaign_name = adwmain.sandbox.campaign_eligibility.campaign_name"
+                             " WHERE sandbox.campaign_eligibility.rider_id IS NULL"
+                             " AND sandbox.campaign_eligibility.campaign_group IS NULL"
+                             " AND sandbox.campaign_eligibility.campaign_name IS NULL"
+                             " AND sandbox.campaign_eligibility.lifecycle IS NULL"
+                             " AND sandbox.campaign_eligibility.rider_state IS NULL"
+                             " AND sandbox.campaign_eligibility.start_date IS NULL"
+                             " AND sandbox.campaign_eligibility.end_date IS NULL"
+                             " AND sandbox.campaign_eligibility.variant_group IS NULL"
+                             " AND sandbox.campaign_eligibility.promo_code IS NULL"
+                             " AND sandbox.campaign_eligibility.offer_type IS NULL"
+                             " AND sandbox.campaign_eligibility.campaign_type IS NULL"
+                             " AND sandbox.campaign_eligibility.email IS NULL"
+                             " AND sandbox.campaign_eligibility.push IS NULL"
+                             " AND sandbox.campaign_eligibility.sms IS NULL"
+                             " AND sandbox.campaign_eligibility.inapp IS NULL"
+                             " AND sandbox.campaign_eligibility.est_cost_cents IS NULL"
+                             " AND sandbox.campaign_eligibility.trigger_ts IS NULL"
+                             " AND sandbox.campaign_eligibility.variant_desc IS NULL"
+                             " AND sandbox.campaign_eligibility.city_id IS NULL"
+                             " AND adwmain.sandbox.campaign_eligibility.promo_campaign_id IS NULL"
+                             " AND adwmain.sandbox.campaign_eligibility.promo_code_id IS NULL"
+                             " AND adwmain.sandbox.campaign_eligibility.segmentation IS NULL"
+                             " AND adwmain.sandbox.campaign_eligibility.campaign_id IS NULL"
+                             " AND sandbox.campaign_eligibility.owner IS NULL;", connection)
+print(sandbox_alooma)
+
 print("okay")
